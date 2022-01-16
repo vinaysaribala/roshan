55594fa84d16b91fa3f615ec1032f6d54db0a4d1
Vinay Saribala <vinay.saribala@gmail.com>
2022-01-16 16:09:33 -0500

    helpsilpa: add a method

--- src/app/service/entitlement.service.ts
+++ src/app/service/entitlement.service.ts
@@ -12,6 +12,10 @@ export class EntitlementService {
   public get(): Observable<any> {
     return this.httpService.get(this.endPoint);
   }
+
+  public post(): Observable<any> {
+    return this.httpService.post('test/silpa', this.endPoint);
+  }
   
   public getList(query: string): Observable<any> {
     return this.httpService.get(`${this.endPoint}/${query}`);
